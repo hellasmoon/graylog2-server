3d7979139712d4a067b82b4d424fe190a9d3c55c
gengxiaotian <gengxiaotian@jd.com>
2017-10-24 15:59:44 +0800

    group add, group item add demo

--- graylog2-web-interface/config.js
+++ graylog2-web-interface/config.js
@@ -1,5 +1,5 @@
 window.appConfig = {
-  gl2ServerUrl: 'http://localhost:9000/api',
+  gl2ServerUrl: 'http://0.0.0.0:9000/api',
   gl2AppPathPrefix: '',
-  rootTimeZone: 'Europe/Berlin',
+  rootTimeZone: 'Asia/Shanghai',
 };
